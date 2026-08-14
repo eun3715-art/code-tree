acf3a5eff2c8a4c1651489f8f4c962085ee11a61
PARKTAEEUN <eun3715@g.postech.edu>
2026-08-14 12:56:12 +0900

    [Passed] 꼬리잡기놀이 (2ms, 0MB)

--- samsung-sw/꼬리잡기놀이/tail-catch-play.cpp
+++ samsung-sw/꼬리잡기놀이/tail-catch-play.cpp
@@ -2,98 +2,48 @@
 #include <vector>
 #include <queue>
 #include <tuple>
+#include <algorithm>
 
 
 using namespace std;
-///////////////////////
+//////////////////////////////////
 int N,M,K;
-//0 빈칸, 1 머리, 2 중간, 3 꼬리, 4는 선
-int board[30][30];
 
-int group[30][30];
+vector<vector<int>> board;
 
-int dist[30][30];;
+vector<vector<pair<int,int>>> team;
 
-//
-struct Eun
-{
-    //그룹 번호 
-    int group_num;
-
-    //그룹 명수
-    int eun_num;
-
-    //머리
-    int r1,c1;
-
-    //꼬리
-    int r2,c2;
-};
-vector<Eun> eun;
+bool visited[30][30];
 
 int dr[4] = {-1,1,0,0};
 int dc[4] = {0,0,-1,1};
 
-
-bool visited[30][30];
-
 int turn;
 
 int score=0;
-/////////////////////
-
+////////////////////////////////////////
 /*
-보드에 0 빈칸, 1 머리, 2 중간, 3 꼬리, 4는 선 계속 갱신.
+0. 초기
+-이차원 벡터1 에 일단 0,1,2,3 저장
+-순회하면서 벡터1이 1인곳에 도달하면 이차원벡터2에 팀 하나 추가.
+-각 팀에서 dfs진행해서 모든 팀원들 머리부터 순서대로 push_back
 
-0. group 연결요소 갱신
+-보드에 0빈칸, 1머리, 2몸통, 3꼬리, 4이동선 -> 0과 4, 1(사람)으로 통일해서 저장
 
-그룹에 그룹 숫자로 격자 채워놓자. 그래서 공이 온 행에서 board탐색햇을때 1,2,3 중에 처음으로 나온 위치의 그룹 번호로 점수 갱신
-
-struct로 팀 나누기. -> 방향 갱신, 팀원들 숫자 저장
-
-1번에서 시작해서 매번 dfs돌려서 dist 배열 만듦
--> 팀원 숫자만큼인 dist가 떨어진 곳은 1로, 1만큼 dist는 3으로. 그 사이는 다 2. 원래 3이 잇던 자리는 4로 바꿔야함
 
 1. 한칸 이동
-- dfs) 머리에서 시작해서 4가 잇는 방향으로 상화좌우 순서대로 4가 잇는 쪽으로 계속 dfs하면서 dist 값 채워넣는다. 
+- 머리에서 시작해서 몸통 방향이 아닌 방향으로 한칸 진행해서 그 좌표를 반환(0이면 continue, 나머지들 중에 머리 다음 좌표가 아닌 쪽으로 이동(몸통이 아닌)
+- 그 좌표를 insert하고 board는 1로 바꿈 맨뒤는 pop_back하고 그 board값은 4로 변경
 
-- 이 dist값대로 보드의 숫자 바꿈(이동). & 구조체 갱신
-
-2. 공 던지기
-- 총 4n라운드. 전역변수 turn을 하나씩 main에서 ++한다. turn % (4n) 만큼 돌린다. 
-
-- 각 단계별 공이 던져지는 격자 칸들을 순서대로 v에 push_back.turn을 변수로 받아서 범위 나눠서
+2. 공던지기
+- 4N으로 나눈 값으로. 각 값마다 v에 해당 격자를 순서대로 저장하고 반환
+- 하나씩 꺼내면서 그 격자 값의 board가 1이면 바로 반환하면서 종료, 없으면 -1,-1반환
 
 3. 공 맞기
--v에서 하나씩 for문에서 꺼내면서 가장 먼저 보드값이 1,2,3중에 하나에 걸리는 곳의 위치 반환
+- -1,-1이 아니면 그 보드 값이 어느 팀소속인지 찾는다. 그리고 그 팀원의 인덱스(몇번째인지) 찾고 score계산. & 뒤집기
 
--그 위치의 그룹 번호 반환받고 그 그룹의 1번 위치에서 이번에 2방향으로 dfs진행. 그러다가 원하는 목적인 위 위치에 도달하면 정지. 그떄의 dist값 반환.
-
--위 dist값으로 점수 더함. 그룹의 머리, 꼬리 바꾸기
-
--
 */
-void dist_reset()
-{
-    for(int i=1; i<=N; i++)
-    {
-        for(int j=1; j<=N; j++)
-        {
-            dist[i][j] =-1;
-        }
-    }
-}
-
-void visited_reset()
-{
-    for(int i=1; i<=N; i++)
-    {
-        for(int j=1; j<=N; j++)
-        {
-            visited[i][j]=false;
-        }
-    }
-}
+////////////////////////////////////////////////////////
 
 int inrange(int newr, int newc)
 {
@@ -104,25 +54,63 @@ int inrange(int newr, int newc)
     return 1;
 }
 
-int dfs1(int r, int c, int id, int &count_eun)
+void dfs(int r, int c)
 {
-    if(board[r][c]==1)
-    {
-        eun[id].r1=r;
-        eun[id].c1=c;
-        count_eun++;
-    }
-
-    if(board[r][c]==3)
-    {
-        eun[id].r2=r;
-        eun[id].c2=c;
-        count_eun++;
-    }
-
-    group[r][c]=id;
     visited[r][c]=true;
 
+    if(board[r][c] != 4)
+    {
+        team.back().push_back({r,c});
+    }
+
+    for(int i=0; i<4; i++)
+    {
+        int newr = r + dr[i];
+        int newc = c + dc[i];
+
+        //칸 넘어감
+        if(!inrange(newr, newc))
+        {
+            continue;
+        }
+        //빈칸임
+        if(board[newr][newc]==0)
+        {
+            continue;
+        }
+        //사람아니면 4가 남는다. 이중에서 사람만 담아야함-> 맨 위에서. 그럼 난 순서를 신경써야 함. 1다음 몸통 쪽으로 이동해야함
+        if((board[newr][newc]==2 && board[r][c]==1) || (!visited[newr][newc]&& board[r][c]!=1))
+        {
+            dfs(newr, newc);
+        }
+    }
+}
+
+void step0()
+{
+    for(int i=1; i<=N; i++)
+    {
+        for(int j=1; j<=N; j++)
+        {
+            if(board[i][j]==1)
+            {
+                team.push_back({});
+                dfs(i,j);
+            }
+        }
+    }
+
+    for(vector<pair<int,int>> v : team)
+    {
+        for(pair<int,int> p : v)
+        {
+            board[p.first][p.second] =1;
+        }
+    }
+}
+/////////////////////////////////
+pair<int,int> move_one(int r, int c, int rr, int cc)
+{
     for(int i=0; i<4; i++)
     {
         int newr = r + dr[i];
@@ -133,179 +121,46 @@ int dfs1(int r, int c, int id, int &count_eun)
             continue;
         }
 
-        if(visited[newr][newc])
+        if(board[newr][newc]==0)
         {
             continue;
         }
 
-        if(board[newr][newc]!=0)
+        if(newr==rr && newc==cc)
         {
-            if(board[newr][newc]==2)
-            {
-                count_eun++;
-            }
-            dfs1(newr, newc, id, count_eun);
-            
+            continue;
         }
+
+        return {newr, newc};
     }
 
-    return count_eun;
-}
-
-void step0()
-{
-    visited_reset();
-
-    int id=1;
-
-    for(int i=1; i<=N; i++)
-    {
-        for(int j=1; j<=N; j++)
-        {
-            int nn=0;
-            if(visited[i][j])
-            {
-                continue;
-            }
-
-            if(board[i][j]==0)
-            {
-                continue;
-            }
-
-            int eun_num = dfs1(i, j, id, nn);
-
-            eun[id].group_num = id;
-            eun[id].eun_num = eun_num;
-
-            id++;
-        }
-    }
-}
-/////////////////////////////////
-void bfs1(int group_num)
-{
-    dist_reset();
-
-    int rr = eun[group_num].r1;
-    int cc = eun[group_num].c1;
-
-    queue<pair<int,int>> q;
-
-    q.push({rr,cc});
-
-    dist[rr][cc] = 0;
-
-    while(!q.empty())
-    {
-        pair<int,int> p = q.front();
-        q.pop();
-
-        int r = p.first;
-        int c = p.second;
-
-        for(int i=0; i<4; i++)
-        {
-            int newr = r + dr[i];
-            int newc = c + dc[i];
-
-            if(!inrange(newr, newc))
-            {
-                continue;
-            }
-
-            if(group[newr][newc] != group_num)
-            {
-                continue;
-            }
-
-            if(dist[newr][newc]!=-1)
-            {
-                continue;
-            }
-
-            if(board[newr][newc]==4 && (board[r][c]==1 || board[r][c]==4))
-            {
-                q.push({newr,newc});
-                dist[newr][newc] = dist[r][c] +1;
-            }
-
-            if(board[newr][newc]==3 && board[r][c]==4)
-            {
-                q.push({newr,newc});
-                dist[newr][newc] = dist[r][c] +1;
-            }
-
-            if(board[newr][newc]==2 && board[r][c]==3)
-            {
-                q.push({newr,newc});
-                dist[newr][newc] = dist[r][c] +1;
-            }
-
-            if(board[newr][newc]==2 && board[r][c]==2)
-            {
-                q.push({newr,newc});
-                dist[newr][newc] = dist[r][c] + 1;
-            }
-
-            if(board[newr][newc]==3 && board[r][c]==1)
-            {
-                q.push({newr,newc});
-                dist[newr][newc] = dist[r][c] +1;
-            }
-        }
-    }
-}
-
-void move_next(int group_num)
-{
-    int cur_dist = dist[eun[group_num].r2][eun[group_num].c2];
-
-    for(int i=1; i<=N; i++)
-    {
-        for(int j=1; j<=N; j++)
-        {
-            if(dist[i][j]==-1)
-            {
-                continue;
-            }
-
-            if(dist[i][j] == cur_dist+1)
-            {
-                if(cur_dist != 1)
-                {
-                    board[eun[group_num].r2][eun[group_num].c2]=4;
-                }
-                eun[group_num].r2 = i;
-                eun[group_num].c2 = j;
-                board[eun[group_num].r2][eun[group_num].c2]=3;
-            }
-
-            if(dist[i][j]==1)
-            {
-                eun[group_num].r1 = i;
-                eun[group_num].c1 = j;
-                board[i][j]=1;
-            }
-
-            if(dist[i][j]==0 || dist[i][j]>cur_dist+1)
-            {
-                board[i][j]=2;
-            }
-        }
-    }
+    return {-1,-1};
 }
 
 void step1()
 {
-    for(int i=1; i<=M; i++)
+    for(int i=0; i<team.size(); i++)
     {
-        bfs1(i);
-        move_next(i);
+        int r = team[i][0].first;
+        int c = team[i][0].second;
+        int rr = team[i][1].first;
+        int cc = team[i][1].second;
+        int lastr = team[i].back().first;
+        int lastc = team[i].back().second;
+
+        pair<int,int> p = move_one(r,c,rr,cc);
+
+        board[lastr][lastc]=4;
+        team[i].pop_back();
+        team[i].insert(team[i].begin(), p);
+
+        board[p.first][p.second]=1;
     }
 }
-////////////////////////////////////
-vector<pair<int,int>> step2()
+
+///////////////////////////////
+
+vector<pair<int,int>> throw_ball()
 {
     int t = (turn - 1) % (4*N) + 1;
 
@@ -346,18 +201,16 @@ vector<pair<int,int>> step2()
     return v;
 }
 
-
-/// //////////////////////////////
-pair<int,int> who()
+pair<int,int> step2()
 {
-    vector<pair<int,int>> v = step2();
+    vector<pair<int,int>> v = throw_ball();
 
     for(pair<int,int> p : v)
     {
         int r = p.first;
         int c = p.second;
 
-        if(board[r][c]==1 || board[r][c]==2 || board[r][c]==3)
+        if(board[r][c]==1)
         {
             return p;
         }
@@ -365,105 +218,52 @@ pair<int,int> who()
     return {0,0};
 }
 
-void bfs2(int group_num)
+////////////////////////////////
+pair<int,int> what_team(int r, int c)
 {
-    dist_reset();
-
-    int r = eun[group_num].r1;
-    int c = eun[group_num].c1;
-
-    queue<pair<int,int>> q;
-
-    q.push({r,c});
-
-    dist[r][c] = 1;
-
-    while(!q.empty())
+    for(int i=0; i<team.size(); i++)
     {
-        pair<int,int> p = q.front();
-        q.pop();
-
-        int rr = p.first;
-        int cc = p.second;
-
-
-        for(int i=0; i<4; i++)
+        for(int j=0; j<team[i].size(); j++)
         {
-            int newr = rr + dr[i];
-            int newc = cc + dc[i];
-
-            if(!inrange(newr, newc))
+            if(team[i][j].first == r && team[i][j].second == c)
             {
-                continue;
-            }
-
-            if(dist[newr][newc]!=-1)
-            {
-                continue;
-            }
-
-            if(group[newr][newc] != group_num)
-            {
-                continue;
-            }
-
-           if(board[newr][newc]==2 && (board[rr][cc]==1 || board[rr][cc]==2))
-            {
-                q.push({newr,newc});
-
-                dist[newr][newc]=dist[rr][cc] +1;
-            }
-
-            if(board[newr][newc]==3 && board[rr][cc]==2)
-            {
-                q.push({newr,newc});
-
-                dist[newr][newc]=dist[rr][cc] +1;
+                return {i,j+1};
             }
         }
     }
+    return {-1,-1};
 }
 
 void step3()
 {
-    pair<int,int> p = who();
+    pair<int,int> p = step2();
 
-    if(p.first==0 && p.second==0)
+    if(p.first == 0 && p.second == 0)
     {
         return;
     }
 
-    int num = group[p.first][p.second];
+    pair<int,int> pp = what_team(p.first, p.second);
 
-    bfs2(num);
+    int cur_score=pp.second;
+    int team_num = pp.first;
 
-    int k = dist[p.first][p.second];
+    score += cur_score*cur_score;
 
-    score = score + k*k;
-
-    int r1 = eun[num].r1;
-    int c1 = eun[num].c1;
-
-    int r2 = eun[num].r2;
-    int c2 = eun[num].c2;
-
-    eun[num].r1=r2;
-    eun[num].c1=c2;
-
-    eun[num].r2=r1;
-    eun[num].c2=c1;
-
-    board[eun[num].r1][eun[num].c1] =1;
-    board[eun[num].r2][eun[num].c2] =3;
+    reverse(team[team_num].begin(), team[team_num].end());
 }
 
+
+///////////////////////////////////////////////////////
 
 int main() 
 {
     int n;
 
     cin >> N >> M >> K;
-    eun.resize(M+1);
+    board.assign(N+1, vector<int>(N+1));
+
+
 
     for(int i=1; i<=N; i++)
     {
@@ -483,9 +283,5 @@ int main()
         step1();
         step3();
     }
-
-    cout << score;
-
-    return 0;
+    cout<< score;
 }
-
