8c0388882f270a72de0af97a009fdf7cde9c499c
PARKTAEEUN <eun3715@g.postech.edu>
2026-09-05 01:22:13 +0900

    [Passed] 아기 고래의 첫 항해 (13ms, 0MB)

--- samsung-sw/아기 고래의 첫 항해/baby-whale-first-voyage.cpp
+++ samsung-sw/아기 고래의 첫 항해/baby-whale-first-voyage.cpp
@@ -1,339 +1,510 @@
-#include <iostream>
-#include <cmath>
-#include <vector>
-#include <queue>
+#include<iostream>
+#include<cstdio>
+#include<queue>
+#include<set>
+#include<vector>
+
+//////////////////////////////////////////////
 
 using namespace std;
-////////////////////////////////
-int board[55][55];
-bool visited[55][55] = {false}; //실제로 방문한 노드
-int dist[55][55]; //시작점부터 해당점까지 거리
-vector<pair<int, int>> path;
-int cur_count = 1;
+//////////////////////////////////////////////
 
-//d값에 따라 현재 좌표값 (r,c) 갱신하고, visited 갱신
-void update_point(int d, int &r, int &c){
-    if(d==1){
-        visited[--r][c] = true;
-        path.push_back({r,c});
-        cur_count++;
-    }
-    else if(d==2){
-        visited[++r][c] = true;
-        path.push_back({r,c});
-        cur_count++;
-    }
-    else if(d==3){
-        visited[r][--c] = true;
-        path.push_back({r,c});
-        cur_count++;
-    }
-    else{
-        visited[r][++c] = true;
-        path.push_back({r,c});
-        cur_count++;
-    }
-} //방향대로 이동한 이후에 좌표값 갱신
+int N;
+int r,c,d;
 
-//1단계로 안될 떄 bfs로 도달 할 수 있는 모든 좌표들의 dist 계산
-void bfs(int N, int r, int c){
-    // 선언
-    queue<pair<int,int>> q;
-    bool bfs_visited[55][55]; // 실제 방문 여부와 관계없이 bfs에서 지나간 노드
+int board[60][60];
+int visited[60][60];
+int dist[60][60];
 
-    // 매번 bfs 초기화
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
-            bfs_visited[i][j] = false;
-            dist[i][j] = -1; // 모든 거리를 -1로 초기화 시켜야 안 겹침
-        }
-    }
+int sea_count;
 
-    // 첫 큐 초기화
-    q.push({r,c});
-    bfs_visited[r][c] = true;
-    dist[r][c] = 0;
+//좌하우상
+int dr[4] = {0,1,0,-1};
+int dc[4] = {-1,0,1,0};
 
-    // 행, 열 인덱스 업데이트
-    int update_row[4] = {0, -1, 0, 1};
-    int update_col[4] = {-1, 0, 1, 0};
+vector<pair<int,int>> v;
 
-    while(!q.empty()){
-        // 현재 큐에서 출발점이 되는 맨앞 요소를 꺼냄
-        pair<int,int> cur = q.front();
-        q.pop();
+pair<int,int> next_sea;
 
-        // front의 행, 열 좌표
-        int cur_row = cur.first;
-        int cur_col = cur.second;
+//////////////////////////////////////////////
 
-        // front 기준으로 상하좌우 확인
-        for(int i = 0; i<4; i++){
-            int next_row = cur_row + update_row[i];
-            int next_col = cur_col + update_col[i];
+/*
+0. 
+전역에 r,c,d 선언해놓고 매번 업뎃하기.
+함수에서는 한 라운드를 구성하고 메인해서 다 도달할떄까지 while 돌리고 그때의 r,c로 담아놓자
+처음에 바다 개수 받아놓고 방문할떄마다 카운트 하나씩 늘리자 -> 종료 조건
 
-            // 격자 범위 내에 있는지 확인
-            if(next_row < 1 || next_row > N || next_col < 1 || next_col > N){
-                continue;
-            }
+1. 인접 탐험
+직진 - 좌 - 우 - 180
+방문햇으면 visited에 1로 바꾸기
 
-            // 암초인지 확인
-            if(board[next_row][next_col] == 1){
-                continue;
-            }
+2. 가장 가까운 바다
+bfs로 dist 배열 생성하고 가장 작은 값도 리턴 -> 암초는 못지나가고 이미 방문한 건 상관없음
+그 작은 값에 해당하는 칸을 dist 이중 포문 돌면서 set에 행, 열 순으로 저장하고 set 리턴
+리턴받은 set 맨 앞에 꺼 추출
+좌하우상 순서로 dist 하나 줄어드는 곳으로 먼저 택해지는 곳으로 점점 이동-> 이동할떄 좌표랑 d같이 갱신
 
-            // 이미 bfs로 방문한 바다면 통과
-            if(bfs_visited[next_row][next_col]){
-                continue;
-            }
-
-            // 세 조건 통과하면 이건 아직 bfs로 방문하지 않은 지나갈 수 있는 바다임
-            bfs_visited[next_row][next_col] = true;
-            dist[next_row][next_col] = dist[cur_row][cur_col] + 1;
-            q.push({next_row, next_col});
-        }
-    }
+*/
+//////////////////////////////////////////////
+int inrange(int r1, int c1)
+{
+    return (r1>=1 &&r1<=N && c1>=1 && c1<=N);
 }
 
-//bfs로 계산한 dist 중 조건 만족하는대로 1순위 도착지 찾음
-void find_nearest_point(int N, int &bestR, int &bestC, int r, int c){
-
-    //dist 갱신
-    bfs(N, r, c);
-
-    int bestD = 1000000;
-
-    for(int i = 1; i<=N; i++){
-        for(int j=1; j<=N; j++){
-
-            //암초인 거랑 이미 방문했었던 노드는 제외
-            if(board[i][j] == 1 || visited[i][j]){
-                continue;
-            }          
-            //아까 dist 초기값이 -1이었으니까 그대로 유지되는 건 탈락 -> 갱신 안된 애들(큐에 못 넣은 애들)
-            else if(dist[i][j] == -1){
-                continue;
-            }
-
-            if(dist[i][j] < bestD){
-                bestD = dist[i][j];
-                bestR = i;
-                bestC = j;
-            }
-
-            else if(dist[i][j] == bestD){
-                if(i<bestR){
-                    bestR = i;
-                    bestC = j;
-                }
-                else if(i==bestR){
-                    if(j<bestC){
-                        bestR = i;
-                        bestC =j;
-                    }
-                }
-            }
-        }
-    }
-}
-
-//출발지에서 도착지까지 이동 경로 저장
-void move_to_destination(int bestR, int bestC, int &r, int &c, int N, int &d){
-
-    bfs(N, bestR, bestC);
-    
-    int move_row[] = {0, 1, 0, -1};
-    int move_col[] = {-1, 0, 1, 0};
-    int move_d[]= {3, 2, 4, 1};
-
-    while(r != bestR || c != bestC)
+int can_move(int r1, int c1)
+{
+    if(!inrange(r1,c1))
     {
-        for(int i =0; i<4; i++)
-        {
-            int new_row = r + move_row[i];
-            int new_col = c + move_col[i];
-
-            //격자 범위 탈주 시 제외
-            if(new_row < 1 || new_row > N || new_col < 1 || new_col > N)
-            {
-                continue;
-            }
-
-            //바다인지 확인
-            if(board[new_row][new_col] == 1)
-            {
-                continue;
-            }
-
-            //거리가 1줄어드는지
-            if(dist[new_row][new_col] != dist[r][c] - 1)
-            {
-                continue;
-            }
-
-            r = new_row;
-            c = new_col;
-            d = move_d[i];
-
-            break;
-        }
-        if(!visited[r][c])
-        {
-            path.push_back({r,c});
-            visited[r][c] = true;
-            cur_count++;
-        }
+        return 0;
     }
+
+    if(visited[r1][c1]==1 || board[r1][c1]==1)
+    {
+        return 0;
+    }
+
+    return 1;
 }
 
-//1단계: 인접탐험. 우선순위에 따라 인접한 지역으로 이동함. 
-void next_point(int &r, int &c, int &d, int &bestR, int &bestC, int N){
+int move_straight()
+{
+    int newr, newc;
+
     if(d==1)
     {
-        if(r-1>=1 && board[r-1][c]==0 && !visited[r-1][c]){
-            update_point(d, r, c);
-            return;
-        }
-        else if(c-1>=1 && board[r][c-1]==0 && !visited[r][c-1]){
-            d=3;
-            update_point(d, r, c);
-            return;
-        }
-        else if(c+1<=N && board[r][c+1]==0 && !visited[r][c+1]){
-            d=4;
-            update_point(d, r, c);
-            return;
-        }
-        else if(r+1<=N && board[r+1][c]==0 && !visited[r+1][c]){
-            d=2;
-            update_point(d, r, c);
-            return;
-        }
-        else{
-            find_nearest_point(N, bestR, bestC, r, c);
-            move_to_destination(bestR, bestC, r, c, N, d);
-            return;
-        }
+        newr = r + dr[3];
+        newc = c + dc[3];
     }
-    if(d==2)
+
+    else if(d==2)
     {
-        if(r+1<=N && board[r+1][c]==0 && !visited[r+1][c]){
-            update_point(d, r, c);
-            return;
-        }
-        else if(c+1<=N && board[r][c+1]==0 && !visited[r][c+1]){
-            d=4;
-            update_point(d, r, c);
-            return;
-        }
-        else if(c-1>=1 && board[r][c-1]==0 && !visited[r][c-1]){
-            d=3;
-            update_point(d, r, c);
-            return;
-        }
-        else if(r-1>=1 && board[r-1][c]==0 && !visited[r-1][c]){
-            d=1;
-            update_point(d, r, c);
-            return;
-        }
-        else{
-            find_nearest_point(N, bestR, bestC, r, c);
-            move_to_destination(bestR, bestC, r, c, N, d);
-            return;
-        }
+        newr = r + dr[1];
+        newc = c + dc[1];
     }
-    if(d==3)
+
+    else if(d==3)
     {
-        if(c-1>=1 && board[r][c-1]==0 && !visited[r][c-1]){
-            update_point(d, r, c);
-            return;
-        }
-        else if(r+1<=N && board[r+1][c]==0 && !visited[r+1][c]){
-            d=2;
-            update_point(d, r, c);
-            return;
-        }
-        else if(r-1>=1 && board[r-1][c]==0 && !visited[r-1][c]){
-            d=1;
-            update_point(d, r, c);
-            return;
-        }
-        else if(c+1<=N && board[r][c+1]==0 && !visited[r][c+1]){
-            d=4;
-            update_point(d, r, c);
-            return;
-        }
-        else{
-            find_nearest_point(N, bestR, bestC, r, c);
-            move_to_destination(bestR, bestC, r, c, N, d);
-            return;
-        }
+        newr = r + dr[0];
+        newc = c + dc[0];
     }
-    if(d==4)
+
+    else
     {
-        if(c+1<=N && board[r][c+1]==0 && !visited[r][c+1]){
-            update_point(d, r, c);
-            return;
-        }
-        else if(r-1>=1 && board[r-1][c]==0 && !visited[r-1][c]){
-            d=1;
-            update_point(d, r, c);
-            return;
-        }
-        else if(r+1<=N && board[r+1][c]==0 && !visited[r+1][c]){
-            d=2;
-            update_point(d, r, c);
-            return;
-        }
-        else if(c-1>=1 && board[r][c-1]==0 && !visited[r][c-1]){
-            d=3;
-            update_point(d, r, c);
-            return;
-        }
-        else{
-            find_nearest_point(N, bestR, bestC, r, c);
-            move_to_destination(bestR, bestC, r, c, N, d);
-            return;
+        newr = r + dr[2];
+        newc = c + dc[2];
+    }
+
+    if(!can_move(newr,newc))
+    {
+        return 0;
+    }
+
+    r=newr;
+    c=newc;
+
+    return 1;
+}
+
+int move_left()
+{
+    int newr, newc, newd;
+
+    if(d==1)
+    {
+        newr = r + dr[0];
+        newc = c + dc[0];
+        newd=3;
+    }
+
+    else if(d==2)
+    {
+        newr = r + dr[2];
+        newc = c + dc[2];
+        newd=4;
+    }
+
+    else if(d==3)
+    {
+        newr = r + dr[1];
+        newc = c + dc[1];
+        newd=2;
+    }
+
+    else
+    {
+        newr = r + dr[3];
+        newc = c + dc[3];
+        newd=1;
+    }
+
+    if(!can_move(newr,newc))
+    {
+        return 0;
+    }
+
+    r=newr;
+    c=newc;
+    d=newd;
+
+    return 1;
+}
+
+int move_right()
+{
+    int newr, newc, newd;
+
+    if(d==1)
+    {
+        newr = r + dr[2];
+        newc = c + dc[2];
+        newd=4;
+    }
+
+    else if(d==2)
+    {
+        newr = r + dr[0];
+        newc = c + dc[0];
+        newd=3;
+    }
+
+    else if(d==3)
+    {
+        newr = r + dr[3];
+        newc = c + dc[3];
+        newd=1;
+    }
+
+    else
+    {
+        newr = r + dr[1];
+        newc = c + dc[1];
+        newd=2;
+    }
+
+    if(!can_move(newr,newc))
+    {
+        return 0;
+    }
+
+    r=newr;
+    c=newc;
+    d=newd;
+
+    return 1;
+}
+
+int move_180()
+{
+    int newr, newc, newd;
+
+    if(d==1)
+    {
+        newr = r + dr[1];
+        newc = c + dc[1];
+        newd=2;
+    }
+
+    else if(d==2)
+    {
+        newr = r + dr[3];
+        newc = c + dc[3];
+        newd=1;
+    }
+
+    else if(d==3)
+    {
+        newr = r + dr[2];
+        newc = c + dc[2];
+        newd=4;
+    }
+
+    else
+    {
+        newr = r + dr[0];
+        newc = c + dc[0];
+        newd=3;
+    }
+
+    if(!can_move(newr,newc))
+    {
+        return 0;
+    }
+
+    r=newr;
+    c=newc;
+    d=newd;
+
+    return 1;
+}
+
+void step1()
+{   
+    if(move_straight()==1 || move_left()==1 || move_right()==1 || move_180()==1)
+    {
+        visited[r][c]=1;
+        sea_count--;
+        v.push_back({r,c});
+
+        step1();
+    }
+
+    return;
+}
+
+
+void reset_dist()
+{
+    for(int i=1; i<=N; i++)
+    {
+        for(int j=1; j<=N; j++)
+        {
+            dist[i][j]=-1;
         }
     }
 }
 
-int main() {
+int cal_dist()
+{
+    reset_dist();
 
-    int N; //줄개수
-    int r; //r,c 초기 위치
-    int c;
-    int d; //지금 보고잇는 방향
-    int bestR = -1;
-    int bestC = -1;
-    int sea_count = 0;
+    queue<pair<int,int>> q;
+
+    dist[r][c]=0;
+
+    q.push({r,c});
+
+    int min_dist=100000;
+
+    while(!q.empty())
+    {   
+        pair<int,int> p = q.front();
+
+        q.pop();
+
+        for(int i=0; i<4; i++)
+        {
+            int newr = p.first + dr[i];
+            int newc = p.second + dc[i];
+
+            if(!inrange(newr,newc))
+            {
+                continue;
+            }
+
+            if(dist[newr][newc]!=-1 || board[newr][newc]==1)
+            {
+                continue;
+            }
+
+            dist[newr][newc] = dist[p.first][p.second] + 1;
+
+            if(visited[newr][newc]==0 && board[newr][newc]==0)
+            {
+                min_dist = min(min_dist, dist[newr][newc]);
+            }
+
+            q.push({newr, newc});
+        }
+    }
+
+    return min_dist;
+}
+
+void can_sea()
+{
+    int min_dist = cal_dist();
+
+    set<pair<int,int>> s;
+
+    for(int i=1; i<=N; i++)
+    {
+        for(int j=1; j<=N; j++)
+        {
+            if(dist[i][j]==min_dist && visited[i][j]==0 && board[i][j]==0)
+            {
+                s.insert({i,j});
+            }
+        }
+    }
+    pair<int,int> p = *s.begin();
+
+    next_sea.first = p.first;
+    next_sea.second = p.second;
+}
+
+void cal_dist_2()
+{
+    reset_dist();
+
+    queue<pair<int,int>> q;
+
+    dist[next_sea.first][next_sea.second]=0;
+
+    q.push({next_sea.first, next_sea.second});
+
+    while(!q.empty())
+    {
+        pair<int,int> p = q.front();
+
+        q.pop();
+
+        for(int i=0; i<4; i++)
+        {
+            int newr = p.first + dr[i];
+            int newc = p.second + dc[i];
+
+            if(!inrange(newr,newc))
+            {
+                continue;
+            }
+
+            if(dist[newr][newc]!=-1 || board[newr][newc]==1)
+            {
+                continue;
+            }
+
+            dist[newr][newc] = dist[p.first][p.second] + 1;
+
+            q.push({newr, newc});
+        }
+    }
+}
+
+//상하좌우
+
+//좌하우상
+int d_update[4] = {3,2,4,1};
+
+void step2()
+{
+    can_sea();
+    cal_dist_2();
+
+    while(r!=next_sea.first || c!=next_sea.second)
+    {
+        for(int i=0; i<4; i++)
+        {
+            int newr = r + dr[i];
+            int newc = c + dc[i];
+            int newd = d_update[i];
+
+            if(!inrange(newr, newc))
+            {
+                continue;
+            }
+
+            if(board[newr][newc]==1)
+            {
+                continue;
+            }
+
+            if(dist[newr][newc] == dist[r][c]-1)
+            {
+                r = newr;
+                c = newc;
+                d = newd;
+
+                break;
+            }
+        }
+    }
+
+    visited[r][c]=1;
+    sea_count--;
+    v.push_back({r,c});
+}
+
+
+//////////////////////////////////////////////
+void cout_visited()
+{
+    for(int i=1; i<=N; i++)
+    {
+        for(int j=1; j<=N; j++)
+        {
+            cout << visited[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n\n";
+}
+
+void cout_dist()
+{
+    for(int i=1; i<=N; i++)
+    {
+        for(int j=1; j<=N; j++)
+        {
+            cout << dist[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n\n";
+}
+
+//////////////////////////////////////////////
+
+int main(int argc, char** argv)
+{
+    freopen("input.txt", "r", stdin);
+
+    int n;
 
     cin >> N >> r >> c >> d;
 
-    for(int i = 1; i <= N; i++){
-        for(int j = 1; j<=N; j++){
-            cin >> board[i][j];
-            if(board[i][j] == 0) sea_count++;
+    for(int i=1; i<=N; i++)
+    {
+        for(int j=1; j<=N; j++)
+        {
+            cin >> n;
+
+            board[i][j] = n;
+
+            if(n==0)
+            {
+                sea_count++;
+            }
         }
     }
 
-    //처음 (r,c) 초기화값 대입
-    visited[r][c] = true;
-    path.push_back({r,c});
 
-    //bestR,bestC 갱신
 
-    //1단계 반복
-    while(1){
-        if(sea_count == cur_count){
+    ///////////////////////////////////////////////
+
+
+    //처음 위치에 잇는 고래 -> 이동한걸로 쳐야함
+    sea_count--;
+    visited[r][c]=1;
+    v.push_back({r,c});
+
+    ////////////////////////////////////////
+
+
+    while(1)
+    {
+        step1();
+
+        if(sea_count==0)
+        {
             break;
         }
-        next_point(r, c, d, bestR, bestC, N);
+
+        step2();
+
+        if(sea_count==0)
+        {
+            break;
+        }
     }
-    
-    for(int i = 0; i < path.size(); i++){
-        cout << path[i].first << " " << path[i].second << "\n";
+
+    for(int i=0; i<v.size(); i++)
+    {
+        cout << v[i].first << " " << v[i].second << "\n";
     }
-    
-    return 0;
+
+
+
+//////////////////////////////////////
+
+    return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
