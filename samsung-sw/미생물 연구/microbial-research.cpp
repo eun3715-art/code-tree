89f5a601ae016b3598b3361bda2705fb7f890b96
PARKTAEEUN <eun3715@g.postech.edu>
2026-09-08 23:42:14 +0900

    [Passed] 미생물 연구 (11ms, 0MB)

--- samsung-sw/미생물 연구/microbial-research.cpp
+++ samsung-sw/미생물 연구/microbial-research.cpp
@@ -1,288 +1,382 @@
 #include<iostream>
-#include<set>
+#include<cstdio>
 #include<vector>
+#include<set>
 #include<algorithm>
 #include<queue>
 
+////////////////////////////////////////////////////////////////
 using namespace std;
+////////////////////////////////////////////////////////////////
+/*
+0. 입력되는 y좌표를 N-c로 변환하면 똑같이 격자로 보면 됨 -> 좌측 상단 0,0 우측하단 N,N
 
-///선어
-int N;
-int board[20][20];
-bool remain[20][20];
-bool visited[20][20]={0};
-int newboard[20][20]={0};
-int id=0;
-int area[100]={0};
+1. 미생물 투입
+-그냥 그대로 일단 board 갱신
+-컴포넌트 세는 로직 -> bfs : 각 미생물의 그룹이 몇개인지 알수잇음 : 이중 포문 돌면서 모든 칸에 대한 생물의 그룹 개수르 ㄹ세고 각 인덱스에 맞게 배열에 저장. -> visited 처리 잘하기
+-그룹 쪼개진 애들은 모두 모두 board 0으로 갱신
 
-struct bio
+2. 용기 이동
+-이중포문 돌면서 각 미생물 넓이 세서 큰 순대로 set에 (넓이, 인덱스) 순서로 삽입 : 넓이 배열에 각 인덱스의 넓이에 대한것도 만들어 놓자
+-set 순서대로 배치
+-이중벡터로 각 인덱스에 해당하는 것의 정규화된 상대좌표를 저장 -> 왼쪽 하단을 0,N으로 정규화하자. _> 이중 포문 돌릴떄 반대로 하면 됨
+-set에서 한개 꺼내면서 상대좌표를 이용해서 위치 설정 :  각 점의 보드가 0인지 일일이 점검 -> 모든 점이 통과되면 그곳으로 정하기 :
+-벡터에서 다 꺼내면 라운드1 끝
+
+3. 넓이 계산
+-bfs 돌리면서 그 보드가 미생물 인덱스랑 다르다 && 0이 아니다 : 넓이 계산 -> bfs로 visited 해놓고 이미 확인한 곳은 안 건들도록 해야함
+
+/////////////////////////////////////////
+
+set에 아무것도 없는 경우 잘 봐야함 버즈에러 안나게
+초기화 신경 쓰기
+
+/////////////////////////////////////
+*/
+////////////////////////////////////////////////////////////////
+//변수선언
+int N, Q;
+
+struct Bio
 {
-    int r1;
-    int c1;
-    int r2;
-    int c2;
+    int r1,c1,r2,c2;
 };
-////////1단계/////////
-/*
-왼쪽 하단 좌표로 통일
-계속 board를 업데이트.
-컴포넌트 세서 2개 이상이면 삭제.
-겹치는 아이디 set에 저장.
-겹치는 아이디 중 하나씩 꺼내서 겹치는 그게 차지하는 전체 좌표 저장.
-그 좌표들 중 겹치지 않는 부분 remain으로 따로 저장.
-그 remain에서 연결되는 부분을 dfs로 컴포넌트 계산. 2개 이상이면 삭제
-*/
+vector<Bio> bio;
 
-//
-//덮어씌우는
-void appear(int r1, int c1, int r2, int c2, int a)
+int board[20][20];
+int new_board[20][20];
+
+//turn 1부터 하기
+int turn;
+
+int dr[4]={-1,1,0,0};
+int dc[4]={0,0,-1,1};
+
+int visited[20][20];
+int group[60];
+int area[60];
+
+set<pair<int,int>> s;
+
+vector<pair<int,int>> v[60];
+
+int ans;
+
+////////////////////////////////////////////////////////////////
+//함수 제작
+void reset_visited()
 {
-    for(int i=r1; i<r2; i++)
+    for(int i=0; i<=N; i++)
     {
-        for(int j=c1; j<c2; j++)
+        for(int j=0; j<=N; j++)
         {
-            board[i][j] = a;
+            visited[i][j]=0;
         }
     }
 }
 
-//사라지기
-void disappear(vector<pair<int, int>> shape)
+void reset_new_board()
 {
-    for(int i=0; i<shape.size(); i++)
+    for(int i=0; i<=N; i++)
     {
-        board[shape[i].first][shape[i].second] =0;
-    }
-}
-
-//겹치는 아이디 추출
-set<int> makeset(int r1, int c1, int r2, int c2)
-{
-    set<int> s;
-
-    for(int i=r1; i<r2; i++)
-    {
-        for(int j=c1; j<c2; j++)
+        for(int j=0; j<=N; j++)
         {
-            if(board[i][j]!=0)
-            {
-                s.insert(board[i][j]);
-            }
-        }
-    }
-    return s;
-}
-
-//전체 좌표
-vector<pair<int,int>> makevector(int a)
-{
-    vector<pair<int,int>> v;
-
-    for(int i=0; i<N; i++)
-    {
-        for(int j=0; j<N; j++)
-        {
-            if(board[i][j]==a)
-            {
-                v.push_back({i,j});
-            }
-        }
-    }
-
-    return v;
-}
-
-//전체 좌표중 remain
-void cal_remain(int r1, int c1, int r2,  int c2, vector<pair<int,int>> v)
-{
-    int r, c;
-
-    for(int i=0; i<N; i++)
-    {
-        for(int j=0; j<N; j++)
-        {
-            remain[i][j] = false;
-        }
-    }
-
-    for(int i=0; i<v.size(); i++)
-    {
-        r = v[i].first;
-        c = v[i].second;
-
-        if((r>=r1 && r<r2) && (c>=c1 && c<c2))
-        {
-            continue;
-        }
-        remain[r][c] = true;
-    }
-}
-
-//
-void dfs(int r, int c)
-{
-    int dr[4] = {0, 0, -1, 1};
-    int dc[4] = {-1, 1, 0, 0};
-    int newr;
-    int newc;
-
-    visited[r][c]=true;
-
-    for(int i=0; i<4; i++)
-    {
-        newr = r + dr[i];
-        newc = c + dc[i];
-
-        if(newr < 0 || newr>=N || newc < 0 || newc>=N)
-        {
-            continue;
-        }
-
-        if(!visited[newr][newc] && remain[newr][newc])
-        {
-            visited[newr][newc]=true;
-            dfs(newr,newc);
+            new_board[i][j]=0;
         }
     }
 }
 
-//컴포너트 계산
-int cal_component(int a, vector<pair<int,int>> v)
+
+void reset_group()
 {
-    int count=0;
-    int r,c;
-    
-    for(int i=0; i<N; i++)
+    for(int i=1; i<=Q; i++)
     {
-        for(int j=0; j<N; j++)
-        {
-            visited[i][j]=false;
-        }
+        group[i]=0;
     }
-
-    for(int i=0; i<v.size(); i++)
-    {
-        r=v[i].first;
-        c=v[i].second;
-
-        if(remain[r][c] && !visited[r][c])
-        {
-            count++;
-            dfs(r, c);
-        }
-    }
-
-    return count;
 }
 
-//최종
-void first(int r1, int c1, int r2, int c2, int &id)
+void reset_area()
 {
-    for(int a : makeset(r1, c1, r2, c2))
-    {
-       vector<pair<int,int>> v= makevector(a);
-
-       cal_remain(r1, c1, r2, c2, v);
-
-       if(cal_component(a, v)>=2)
-       {
-        disappear(v);
-       }
-
-    }
-
-    appear(r1, c1, r2, c2, ++id);
-}
-////////////////////////////
-
-
-
-///////////2단계/////////
-/*
-용기 이동
-새로운 보드 하나 만들어서 거기다가 새로 채우면 될듯.
-그리고 그 보드를 다음 실험에서의 보드로 취급하게끔.
-선택: 우선순위 큐. 넓이랑 아이디를 set으로 엮어서 큐에다가 저장.
-옮길떄 상대좌표 계산
-v 중에 가장 작은 좌표를 min으로 구하고 모든 좌표에서 그 min값을 뺸다. 그럼 0,0으로 상대좌표가 됨.
-그 상대좌표로 우선순위대로 배열. 이때 겹치는 지, 범위를 넘는지 확인하는 함수 만들자. 그리고 우선순위대로 다 돌앗는데 true못하면 그냥 삭제
-*/
-
-//넓이 계산
-void calc_area()
-{
-    for(int i=1; i<=id; i++)
+    for(int i=1; i<=Q; i++)
     {
         area[i]=0;
     }
+}
 
-    for(int i=0; i<N; i++)
+void reset_v()
+{
+    for(int i=1; i<=Q; i++)
     {
-        for(int j=0; j<N; j++)
+        v[i].clear();
+    }
+}
+
+
+void into()
+{
+    int r1 = bio[turn].r1;
+    int r2 = bio[turn].r2;
+    int c1 = bio[turn].c1;
+    int c2 = bio[turn].c2;
+
+    for(int i=c1; i>c2; i--)
+    {
+        for(int j=r1; j<r2; j++)
         {
-            if(board[i][j]!=0)
+            board[i][j]=turn;
+        }
+    }
+}
+
+int inrange(int r, int c)
+{
+    return (r>=0 && r<=N && c>=0 && c<=N);
+}
+
+void bfs1(int r, int c)
+{
+    queue<pair<int,int>> q; 
+    q.push({r,c});
+    visited[r][c]=1;
+
+    int idx = board[r][c];
+
+    group[idx]++;
+
+    while(!q.empty())
+    {
+        pair<int,int> p = q.front();
+        q.pop();
+
+        for(int i=0; i<4; i++)
+        {
+            int newr = p.first + dr[i];
+            int newc = p.second + dc[i];
+
+            if(!inrange(newr, newc))
             {
-                area[board[i][j]]++;
+                continue;
+            }
+
+            if(visited[newr][newc])
+            {
+                continue;
+            }
+
+            if(board[newr][newc]==idx)
+            {
+                q.push({newr,newc});
+                visited[newr][newc]=1;
             }
         }
     }
 }
 
-//선택
-priority_queue<pair<int,int>> Q()
+void cal_component()
 {
-    priority_queue<pair<int,int>> pq;
+    reset_visited();
+    reset_group();
 
-    calc_area();
-
-    for(int i=1; i<=id; i++)
+    for(int i=0; i<=N; i++)
     {
-        pq.push({area[i], -i});
-    }
+        for(int j=0; j<=N; j++)
+        {
+            if(visited[i][j]==1 || board[i][j]==0)
+            {
+                continue;
+            }
 
-    return pq;
-}
-
-//상대좌표 계산. a가 0일떄 경계
-void coor(vector<pair<int,int>> &v)
-{
-    int r, c;
-    int minr =100;
-    int minc=100;
-
-    for(int i=0; i<v.size(); i++)
-    {
-        r=v[i].first;
-        c=v[i].second;
-
-        minr=min(r, minr);
-        minc=min(c, minc);
-    }
-
-    for(int i=0; i<v.size(); i++)
-    {
-        v[i].first-=minr;
-        v[i].second-=minc;
+            bfs1(i,j);
+        }
     }
 }
 
-//
-int canmove(int r, int c, vector<pair<int,int>> v)
+void removing(int n)
 {
-    for(int i=0; i<v.size(); i++)
+    for(int i=0; i<=N; i++)
     {
-        v[i].first+=r;
-        v[i].second+=c;
+        for(int j=0; j<=N; j++)
+        {
+            if(board[i][j]==n)
+            {
+                board[i][j]=0;
+            }
+        }
+    }
+}
+
+void remove_group()
+{
+    for(int i=1; i<=Q; i++)
+    {
+        if(group[i]>1)
+        {
+            removing(i);
+        }
+    }
+}
+
+void step1()
+{
+    into();
+    cal_component();
+
+    remove_group();
+}
+
+
+///////////////////////
+void bfs2(int r, int c)
+{
+    queue<pair<int,int>> q;
+
+    q.push({r,c});
+    visited[r][c]=1;
+
+    int idx = board[r][c];
+
+    int wide=0;
+
+    while(!q.empty())
+    {
+        pair<int,int> p = q.front();
+        q.pop();
+        wide++;
+
+        for(int i=0; i<4; i++)
+        {
+            int newr = p.first + dr[i];
+            int newc = p.second + dc[i];
+
+            if(!inrange(newr, newc))
+            {
+                continue;
+            }
+
+            if(visited[newr][newc])
+            {
+                continue;
+            }
+
+            if(board[newr][newc]==idx)
+            {
+                visited[newr][newc]=1;
+                q.push({newr,newc});
+            }
+        }
     }
 
-    for(int i=0; i<v.size(); i++)
+    area[idx]=wide;
+
+    s.insert({-wide, idx});
+}
+
+void widest()
+{
+    reset_visited();
+    reset_area();
+    s.clear();
+    
+    for(int i=0; i<=N; i++)
     {
-        if(v[i].first<0 || v[i].first>=N || v[i].second<0 || v[i].second>=N)
+        for(int j=0; j<=N; j++)
+        {
+            if(visited[i][j]==1 || board[i][j]==0)
+            {
+                continue;
+            }
+
+            bfs2(i,j);
+        }
+    }
+}
+
+pair<int,int> normalize(int r, int c, int x, int y)
+{
+    int newr=r+x;
+    int newc=c-y;
+
+    return {newr,newc};
+}
+
+void bfs3(int r, int c)
+{
+    int x,y;
+
+    queue<pair<int,int>> q;
+    int idx = board[r][c];
+
+    q.push({r,c});
+    visited[r][c]=1;
+
+    x=N-r;
+    y=c;
+    pair<int,int> pp = normalize(r, c, x, y);
+    v[idx].push_back({pp.first,pp.second});
+
+    while(!q.empty())
+    {
+        pair<int,int> p = q.front();
+        q.pop();
+
+        for(int i=0; i<4; i++)
+        {
+            int newr = p.first + dr[i];
+            int newc = p.second + dc[i];
+
+            if(!inrange(newr, newc))
+            {
+                continue;
+            }
+
+            if(visited[newr][newc])
+            {
+                continue;
+            }
+
+            if(board[newr][newc]==idx)
+            {
+                visited[newr][newc]=1;
+                q.push({newr,newc});
+
+                pair<int,int> ppp = normalize(newr, newc, x, y);
+                v[idx].push_back({ppp.first, ppp.second});
+            }
+        }
+    }
+
+}
+
+void normal()
+{
+    reset_visited();
+    reset_v();
+    
+    for(int i=N; i>=0; i--)
+    {
+        for(int j=0; j<=N; j++)
+        {
+            if(visited[i][j]==1 || board[i][j]==0)
+            {
+                continue;
+            }
+
+            bfs3(i,j);
+        }
+    }
+}
+
+
+int can_batch(vector<pair<int,int>> new_v)
+{
+    for(pair<int,int> pp : new_v)
+    {
+        if(!inrange(pp.first, pp.second))
         {
             return 0;
         }
 
-        if(newboard[v[i].first][v[i].second]!=0)
+        if(new_board[pp.first][pp.second]!=0)
         {
             return 0;
         }
@@ -291,160 +385,294 @@ int canmove(int r, int c, vector<pair<int,int>> v)
     return 1;
 }
 
-//생기기2
-void appear2(vector<pair<int, int>> &shape, int k, int j, int id)
+void move_one(int r, int c, int idx, vector<pair<int,int>> &new_v)
 {
-    for(int i=0; i<shape.size(); i++)
+    for(pair<int,int> p : v[idx])
     {
-        shape[i].first+=k;
-        shape[i].second+=j;
-    }
-    for(int i=0; i<shape.size(); i++)
-    {
-        newboard[shape[i].first][shape[i].second] = id;
+        new_v.push_back({p.first - r, p.second + c});
     }
 }
 
-//어디에 둘까
-void where(int name)
+
+int move_all(int idx)
 {
-    int finalr=-1;
-    int finalc=-1;
-
-    vector<pair<int,int>> v = makevector(name);
-
-    coor(v);
-
-    for(int j=0; j<N && finalr==-1; j++)
+    for(int i=0; i<=N; i++)
     {
-        for(int i=0; i<N && finalr ==-1; i++)
+        for(int j=0; j<=N; j++)
         {
-            if(canmove(i ,j, v)==1)
+            vector<pair<int,int>> new_v;
+
+            move_one(j, i, idx, new_v);
+
+            if(can_batch(new_v)==1)
             {
-                finalr=i;
-                finalc=j;
-                appear2(v, finalr, finalc, name);
-            }
-        }
-    }
-}
-
-//최종
-void second()
-{
-    priority_queue<pair<int,int>> pq = Q();
-
-    while(!pq.empty())
-    {
-        int area = pq.top().first;
-        int name = -pq.top().second;
-
-        pq.pop();
-
-        where(name);
-    }
-}
-
-///////////////////////////////////
-/*
-3단계
-모든 좌표들 중 상,우 를 확인햇을떄 다른 id가 나오는 경우 true.
-그리고 그 true면 바로 그 쌍은 더이상 저장 안하도록 배열로 중복방지.
-*/
-
-int third()
-{
-    bool checked[100][100]={false};
-    bool visited[20][20]={false};
-    int dr[2]={-1,0};
-    int dc[2]={0,1};
-    long long int result=0;
-
-    for(int i=0; i<N; i++)
-    {
-        for(int j=0; j<N; j++)
-        {
-            for(int k=0; k<2; k++)
-            {
-                int r = i + dr[k];
-                int c = j + dc[k];
-
-                if(r<0||r>=N || c<0 || c>=N)
+                for(int k=0; k<v[idx].size(); k++)
                 {
-                    continue;
+                    v[idx][k]=new_v[k];
                 }
 
-                if(newboard[i][j]==0)
-                {
-                    continue;
-                }
-
-                if(newboard[r][c]!=0)
-                {
-                    if(newboard[i][j]!=newboard[r][c])
-                    {
-                        int a=min(newboard[i][j], newboard[r][c]);
-                        int b=max(newboard[i][j], newboard[r][c]);
-                    
-                        if(!checked[a][b])
-                        {
-                            result += area[newboard[i][j]] * area[newboard[r][c]];
-
-                            checked[a][b]=true;
-                        }
-                    }
-                }
+                return 1;
             }
         }
     }
 
+    return 0;
 
-    for(int i=0; i<N; i++)
-    {
-        for(int j=0; j<N; j++)
-        {
-            board[i][j]=newboard[i][j];
-        }
-    }
-
-    for(int i=0; i<N; i++)
-    {
-        for(int j=0; j<N; j++)
-        {
-            newboard[i][j]=0;
-        }
-    }
-    return result;
 }
 
 
-int main()
+void rebatch_board(int idx)
 {
-    //////선언
-    int Q, r1, r2, c1, c2;
+    for(pair<int,int> p : v[idx])
+    {
+        new_board[p.first][p.second]=idx;
+    }
+}
+
+void board_update()
+{
+    for(int i=0; i<=N; i++)
+    {
+        for(int j=0; j<=N; j++)
+        {
+            board[i][j]=new_board[i][j];
+        }
+    }
+}
+
+
+void step2()
+{   
+    widest();
+
+    normal();
+
+    reset_new_board();
+
+    for(pair<int,int> p: s)
+    {
+        int idx = p.second;
+
+        int n = move_all(idx);
+        
+        if(n==0)
+        {
+            continue;
+        }
+        
+        rebatch_board(idx);
+    }
+
+    board_update();
+}
+
+
+void bfs4(int r, int c)
+{
+    queue<pair<int,int>> q;
+
+    q.push({r,c});
+    visited[r][c]=1;
+
+    set<int> ss;
+
+    int idx = board[r][c];
+
+    while(!q.empty())
+    {
+        pair<int,int> p = q.front();
+        q.pop();
+
+        for(int i=0; i<4; i++)
+        {
+            int newr = p.first + dr[i];
+            int newc = p.second + dc[i];
+
+            if(!inrange(newr, newc))
+            {
+                continue;
+            }
+
+            if(visited[newr][newc])
+            {
+                continue;
+            }
+
+            if(board[newr][newc]==idx)
+            {
+                visited[newr][newc]=1;
+                q.push({newr,newc});
+            }
+
+            else if(board[newr][newc]!=idx && board[newr][newc]!=0)
+            {
+                ss.insert({board[newr][newc]});
+            }
+        }
+    }
+    for(int a : ss)
+    {
+        ans += area[a] * area[board[r][c]];
+    }
+}
+
+void step3()
+{
+    reset_visited();
+
+    for(int i=0; i<=N; i++)
+    {
+        for(int j=0; j<=N; j++)
+        {
+            if(visited[i][j]==1 || board[i][j]==0)
+            {
+                continue;
+            }
+
+            bfs4(i,j);
+        }
+    }
+}
+
+
+////////////////////////////////////////////////////////////////
+
+void cout_board()
+{
+    for(int i=0; i<=N; i++)
+    {
+        for(int j=0; j<=N; j++)
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n\n";
+}
+
+void cout_new_board()
+{
+    for(int i=0; i<=N; i++)
+    {
+        for(int j=0; j<=N; j++)
+        {
+            cout << new_board[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n\n";
+}
+
+void cout_visited()
+{
+    for(int i=0; i<=N; i++)
+    {
+        for(int j=0; j<=N; j++)
+        {
+            cout << visited[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n\n";
+}
+
+void cout_component()
+{
+    for(int i=1; i<=Q; i++)
+    {
+        cout << group[i] << " ";
+    }
+    cout <<"\n\n";
+}
+
+void cout_s()
+{
+    for(pair<int,int> p : s)
+    {
+        cout << p.first << " " << p.second <<"\n"; 
+    }
+
+    cout <<"\n\n";
+}
+
+void cout_v()
+{
+    for(int i=1; i<=Q; i++)
+    {
+        for(pair<int,int> p : v[i])
+        {
+            cout << p.first << p.second << " "; 
+        }
+
+        cout << "\n";
+    }
+
+    cout <<"\n\n";
+}
+
+void cout_group()
+{
+    for(int i=1; i<=Q; i++)
+    {
+        cout << group[i] << " ";
+    }
+
+    cout <<"\n\n";
+}
+
+
+
+
+////////////////////////////////////////////////////////////////
+
+int main(int argc, char** argv)
+{
+    freopen("input.txt", "r", stdin);
+////////////////////////////////////////////////////////////////
+//입력
+    int n;
+    int r1,c1,r2,c2;
     
-
-    ///입력
     cin >> N >> Q;
-    vector<bio> bios(Q);
+    bio.resize(Q+1);
+    N--;
 
-    for(int i=0; i<Q; i++)
+    for(int i=1; i<=Q; i++)
     {
         cin >> r1 >> c1 >> r2 >> c2;
-        bios[i].r1=c1;
-        bios[i].r2=c2;
-        bios[i].c1=r1;
-        bios[i].c2=r2;
+        bio[i].r1=r1;
+        bio[i].r2=r2;
+        bio[i].c1=N-c1;
+        bio[i].c2=N-c2;
     }
 
-    //////함수
-    for(int i=0; i<Q; i++)
+
+////////////////////////////////////////////////////////////////
+//출력
+
+    vector<int> rlt;
+
+    
+    for(turn=1; turn<=Q; turn++)
     {
-        first(bios[i].r1, bios[i].c1, bios[i].r2, bios[i].c2, id);
-        second();
-        cout << third()<<"\n";
+        ans=0;
+
+        step1();
+        step2();
+    
+        step3();
+        
+        rlt.push_back({ans});
     }
 
+    for(int i=0; i<rlt.size(); i++)
+    {
+        cout << rlt[i]<<"\n";
+    }
+
+
+
+////////////////////////////////////////////////////////////////
 
     return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
